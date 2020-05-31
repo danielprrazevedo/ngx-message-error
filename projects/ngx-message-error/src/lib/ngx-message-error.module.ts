862476ed44264f4c04be36f3403a3b9ab25d6611
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2020-05-31 14:50:58 -0300

    fix(): global config service

--- projects/ngx-message-error/src/lib/ngx-message-error.module.ts
+++ projects/ngx-message-error/src/lib/ngx-message-error.module.ts
@@ -1,21 +1,18 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule, ModuleWithProviders, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxMessageErrorComponent } from './ngx-message-error.component';
-import { NgxMessageErrorService, MessagesConfig } from './ngx-message-error.service';
+import { MessagesConfig } from './ngx-message-error.service';
 
 @NgModule({
   declarations: [NgxMessageErrorComponent],
   imports: [CommonModule],
   exports: [NgxMessageErrorComponent],
-  providers: [NgxMessageErrorService]
 })
 export class NgxMessageErrorModule {
-  static config(messages: MessagesConfig): ModuleWithProviders {
+  static forRoot(@Optional() @SkipSelf() messages?: MessagesConfig): ModuleWithProviders {
     return {
       ngModule: NgxMessageErrorModule,
-      providers: [
-        {provide: MessagesConfig, useValue: messages }
-      ]
+      providers: [{ provide: MessagesConfig, useValue: messages }],
     };
   }
 }
--- projects/ngx-message-error/src/public-api.ts
+++ projects/ngx-message-error/src/public-api.ts
@@ -2,4 +2,6 @@
  * Public API Surface of ngx-message-error
  */
 
+export * from './lib/ngx-message-error.service';
+export * from './lib/ngx-message-error.component';
 export * from './lib/ngx-message-error.module';
