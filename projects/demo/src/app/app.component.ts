e0d9c07c8f1a7ec304e32373c413b96dd9bc4b54
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2020-05-31 21:05:55 -0300

    add demo project

---
+++ projects/demo/src/app/app.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  simpleForm: FormGroup;
+  customMessagesForm: FormGroup;
+  customValidationForm: FormGroup;
+
+  codeSimpleForm = `this.simpleForm = this.fb.group({
+  min: ['', [Validators.min(5)]],
+  max: ['', [Validators.max(5)]],
+  required: ['', [Validators.required]],
+  email: ['', [Validators.email]],
+  minlength: ['', [Validators.minLength(5)]],
+  maxlength: ['', [Validators.maxLength(10)]],
+});
+`;
+  codeCustomMessageTs = `this.customMessagesForm = this.fb.group({
+  customMessage: ['', [Validators.required]],
+});`;
+  codeCustomMessageHtml = `<form [formGroup]="customMessagesForm">
+  <fieldset>
+    <legend>Custom Message Validation</legend>
+    <div>
+      <label>
+        Custom Input:<br>
+        <input formControlName="customMessage">
+      </label><br>
+      <ngx-message-error formControlName="customMessage" [messages]="{ required: 'My custom message' }"></ngx-message-error>
+    </div>
+    <button type="submit">Submit</button>
+  </fieldset>
+</form>`;
+  codeCustomValidatorTs = `const equalTo = (value) => ((control: FormControl) => {
+  if (control.value === value) return null;
+  return { equalto: true };
+});
+this.customValidationForm = this.fb.group({
+  customValidation: ['', [equalTo('myform')]],
+});`;
+  codeCustomValidatorHtml = `<form [formGroup]="customValidationForm">
+  <fieldset>
+    <legend>Custom Message Validation</legend>
+    <div>
+      <label>
+        Custom Validation equalto (myform):<br>
+        <input formControlName="customValidation">
+      </label><br>
+      <ngx-message-error formControlName="customValidation" [messages]="{ equalto: 'My custom validation with message' }"></ngx-message-error>
+    </div>
+    <button type="submit">Submit</button>
+  </fieldset>
+</form>`;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.simpleForm = this.fb.group({
+      min: ['', [Validators.min(5)]],
+      max: ['', [Validators.max(5)]],
+      required: ['', [Validators.required]],
+      email: ['', [Validators.email]],
+      minlength: ['', [Validators.minLength(5)]],
+      maxlength: ['', [Validators.maxLength(10)]],
+    });
+    this.customMessagesForm = this.fb.group({
+      customMessage: ['', [Validators.required]],
+    });
+    const equalTo = (value) => ((control: FormControl) => {
+      if (control.value === value) return null;
+      return { equalto: true };
+    });
+    this.customValidationForm = this.fb.group({
+      customValidation: ['', [equalTo('myform')]],
+    });
+  }
+}
---
+++ projects/demo/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import {
+  HighlightModule,
+  HIGHLIGHT_OPTIONS,
+  HighlightOptions,
+} from 'ngx-highlightjs';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { NgxMessageErrorModule } from 'projects/ngx-message-error/src/public-api';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// import hljs from 'highlight.js';
+// document.defaultView['hljs'] = hljs;
+// import 'highlightjs-line-numbers.js';
+
+export function getHighlightLanguages() {
+  return {
+    typescript: () => import('highlight.js/lib/languages/typescript'),
+    css: () => import('highlight.js/lib/languages/css'),
+    xml: () => import('highlight.js/lib/languages/xml'),
+  };
+}
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgxMessageErrorModule.forRoot(),
+    HighlightModule,
+  ],
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        lineNumbers: true,
+        // languages: getHighlightLanguages()
+      } as HighlightOptions,
+    },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
